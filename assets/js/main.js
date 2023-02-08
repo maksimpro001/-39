f24ff4641d40a63aea7b4498d8d82e6a1551ed54
maksimpro001 <ytmaksik425@gmail.com>
2023-02-08 18:45:16 +0200

    master

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function P_S(x, y) {
+  x = prompt("Enter number 1: ");
+  y = prompt("Enter number 2: ");
+  P = (x + y) * 2;
+  S = x * y;
+  alert("Периметер: " + P);
+  alert("Площа: " + S);
+}
+
+P_S(10, 7);
+
+a = prompt("Enter number: ");
+alert((a + 10) * 20 - 30 / 5);
